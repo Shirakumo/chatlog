4adc881e08964d0b6b1dbc60137a52fb4c40a1df
Shinmera <shinmera@tymoon.eu>
2015-11-03 16:22:59 +0100

    Added weechat backporting

--- backport.lisp
+++ backport.lisp
@@ -14,6 +14,9 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
    #:process-with-db))
 (in-package #:chatlog-backport)
 
+(defvar *server* "freenode")
+(defvar *channel* "#lisp")
+
 (defun insert (time server channel nick type message)
   "Perform the actual insert."
   (format T "~&Inserting ~a ~a/~a <~a> [~a] ~a" time server channel nick type message)
@@ -27,7 +30,7 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
     (let ((time (local-time:timestamp-to-unix
                  (local-time:parse-timestring time))))
       (flet ((insert (type message)
-               (insert time "freenode" "#lisp" nick type message)))
+               (insert time *server* *channel* nick type message)))
         (cond (message
                (insert "m" message))
               ((string-equal action "is now known as")
@@ -38,6 +41,20 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
                (insert "j" " ** JOIN"))
               ((string-equal action "left")
                (insert "p" " ** PART")))))))
+
+(defun process-weechat (line)
+  (cl-ppcre:register-groups-bind (time NIL joiner NIL quitter reason NIL oldnick newnick NIL sender message)
+      ("^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\\s(-->\\s([^ ]+) )?(<--\\s([^ ]+) \\(.*?\\) has quit \\((.*)\\)$)?(--\\s([^ ]+) is now known as ([^ ]+))?(>([^>]+)>\\s(.*))?" line)
+    (let ((time (local-time:timestamp-to-unix
+                 ;; Piece of shit timezone missing defaults. Kludge it by assuming it's our timezone too.
+                 (local-time:parse-timestring time :date-time-separator #\  :offset (nth-value 9 (local-time:decode-timestamp (local-time:now)))))))
+      (flet ((insert (type message)
+               (insert time *server* *channel* (or joiner quitter oldnick sender) type message)))
+        (cond (joiner (insert "j" " ** JOIN"))
+              (quitter (insert "q" (format NIL " ** QUIT ~a" reason)))
+              (oldnick (insert "n" (format NIL " ** NICK ~a" newnick)))
+              (sender (insert "m" message)))))
+    (values time joiner quitter reason oldnick newnick sender message)))
 
 (defun process-file (file &key (processor #'process-clozure))
   "Process a log file line by line."
@@ -56,7 +73,11 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
                                   (user "irc")
                                   (pass)
                                   (db "irc")
-                                  (processor #'process-clozure))
+                                  (processor #'process-clozure)
+                                  (server *server*)
+                                  (channel *channel*))
   "Process a log file with the db connection established."
-  (postmodern:with-connection (list db user pass host :port port)
-    (process-file file :processor processor)))
+  (let ((*server* server)
+        (*channel* channel))
+    (postmodern:with-connection (list db user pass host :port port)
+      (process-file file :processor processor))))
